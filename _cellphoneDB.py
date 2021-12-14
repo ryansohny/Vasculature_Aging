62b731d4a86fcb05b42560f98ff05b081de40e39
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-12-14 20:14:12 +0900

    Update _cellphoneDB.py

--- _cellphoneDB.py
+++ _cellphoneDB.py
@@ -1,7 +1,105 @@
-# Using the normalized, non-log transformed data
-df_expr_matrix = integrated.X # integrated.X ==> 지금 sparse matrix가 아니라 numpy.matrix로 되어있음.
-df_expr_matrix = df_expr_matrix.T
-df_expr_matrix = pd.DataFrame(df_expr_matrix)
-df_expr_matrix.columns = test3.obs.index
-df_expr_matrix.set_index(test3.var.index, inplace=True)
+#export PATH=/data/Projects/phenomata/99.Tools/anaconda3/bin:$PATH
+#source activate scanpy_1.8.1
 
+from anndata import AnnData
+import anndata
+from scipy import sparse, io
+import scipy
+import pandas as pd
+import scipy.io
+import os
+import scanpy as sc
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.colors
+matplotlib.use('TkAgg')
+import numpy as np
+import seaborn as sb
+import math
+import scanpy.external as sce
+import scrublet as scr
+
+sc.settings.verbosity = 3
+plt.rcParams['figure.figsize'] = (7,7)
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Arial'
+
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["#104e8b", "#ffdab9", "#8b0a50"])
+
+m01 = sc.read_10x_h5("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/01.Cell-Ranger/01month_filtered_feature_bc_matrix.h5")
+m01.var_names_make_unique()
+
+m10 = sc.read_10x_h5("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/01.Cell-Ranger/10months_filtered_feature_bc_matrix.h5")
+m10.var_names_make_unique()
+
+m20 = sc.read_10x_h5("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/01.Cell-Ranger/20months_filtered_feature_bc_matrix.h5")
+m20.var_names_make_unique()
+
+mito_genes = m01.var_names.str.startswith('mt-')
+m01.obs['percent_mito'] = np.ravel(np.sum(m01[:, mito_genes].X, axis=1)) / np.ravel(np.sum(m01.X, axis=1))
+m10.obs['percent_mito'] = np.ravel(np.sum(m10[:, mito_genes].X, axis=1)) / np.ravel(np.sum(m10.X, axis=1))
+m20.obs['percent_mito'] = np.ravel(np.sum(m20[:, mito_genes].X, axis=1)) / np.ravel(np.sum(m20.X, axis=1))
+
+sc.pp.filter_cells(m01, min_counts=2000)
+sc.pp.filter_cells(m01, min_genes=1500)
+
+sc.pp.filter_cells(m10, min_counts=3000)
+sc.pp.filter_cells(m10, min_genes=1500)
+
+sc.pp.filter_cells(m20, min_counts=3000)
+sc.pp.filter_cells(m20, min_genes=1500)
+
+m01 = m01[m01.obs['percent_mito'] < 0.2]
+m10 = m10[m10.obs['percent_mito'] < 0.2]
+m20 = m20[m20.obs['percent_mito'] < 0.2]
+
+integrated = AnnData.concatenate(m01, m10, m20, join='outer', batch_categories = ['m01', 'm10', 'm20'], index_unique = '-')
+
+sc.pp.filter_genes(integrated, min_cells=5) # integrated.var에 n_cells 추가
+integrated.layers["counts"] = integrated.X.copy()
+integrated.raw = integrated
+
+import rpy2.rinterface_lib.callbacks
+import logging
+from rpy2.robjects import pandas2ri
+import anndata2ri
+pandas2ri.activate()
+anndata2ri.activate()
+%load_ext rpy2.ipython
+%%R
+library(scran)
+library(dplyr)
+
+
+
+
+
+
+%config InlineBackend.figure_format = 'retina'
+
+adata_pp = integrated.copy()
+sc.pp.normalize_per_cell(adata_pp, counts_per_cell_after=1e6)
+sc.pp.log1p(adata_pp) # works on anndata.X
+sc.tl.pca(adata_pp, n_comps=15) ## 여기서 이 n_component의 숫자를 늘리면 size_factors를 estimation하는 데 도움이 될까?
+sc.pp.neighbors(adata_pp)
+sc.tl.leiden(adata_pp, key_added='groups', resolution=0.5)
+input_groups = adata_pp.obs['groups']
+data_mat = integrated.X.T
+%%R -i data_mat -i input_groups -o size_factors
+size_factors = BiocGenerics::sizeFactors(computeSumFactors(SingleCellExperiment::SingleCellExperiment(list(counts=data_mat)), clusters=input_groups, min.mean=0.1))
+
+
+
+del adata_pp
+integrated.obs['size_factors'] = size_factors
+
+integrated.X /= integrated.obs['size_factors'].values[:, None]
+
+# Using the normalized, non-log transformed data
+#df_expr_matrix = integrated.X # integrated.X ==> 지금 sparse matrix가 아니라 numpy.matrix로 되어있음.
+#df_expr_matrix = df_expr_matrix.T
+#df_expr_matrix = pd.DataFrame(df_expr_matrix)
+#df_expr_matrix.columns = test3.obs.index
+#df_expr_matrix.set_index(test3.var.index, inplace=True)
+
+anndata.AnnData(X=integrated.X, obs=test3.obs, var=test3.var).write(filename="test3_CellPhoneDB.h5ad")
