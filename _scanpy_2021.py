cc5c1bf29c0f4c8c95edbc7ac29e375235a217bf
RYAN SOHN <45253545+ryansohny@users.noreply.github.com>
2021-12-28 21:40:33 +0900

    Update _scanpy_2021.py

--- _scanpy_2021.py
+++ _scanpy_2021.py
@@ -17,13 +17,12 @@ import numpy as np
 import seaborn as sb
 import math
 import scanpy.external as sce
-import scrublet as scr
+#import scrublet as scr
 
 sc.settings.verbosity = 3
 plt.rcParams['figure.figsize'] = (7,7)
-plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = 'Arial'
-
+#plt.rcParams['font.family'] = 'sans-serif'
+#plt.rcParams['font.sans-serif'] = 'Arial'
 cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ["#104e8b", "#ffdab9", "#8b0a50"])
 
 test3 = sc.read_h5ad("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/03.Scanpy/test3.h5ad")
@@ -115,7 +114,7 @@ cell_cycle_genes=[x.strip()[0] + x.strip()[1:].lower() for x in open("/data/Proj
 s_genes= cell_cycle_genes[:43]
 g2m_genes= cell_cycle_genes[43:]
 cell_cycle_genes = [x for x in cell_cycle_genes if x in test3.var_names]
-
+sc.tl.score_genes_cell_cycle(test3, s_genes=s_genes, g2m_genes=g2m_genes)
 
 sc.tl.pca(test3, n_comps=100, use_highly_variable=True, svd_solver='arpack')
 matplotlib.use('TkAgg')
@@ -134,7 +133,7 @@ sc.tl.leiden(test3, resolution=0.5, key_added='leiden_r05') #### 0 ~ 13 ==> 2021
 sc.tl.leiden(test3, resolution=1.0, key_added='leiden_r10')
 sc.pl.umap(test3, color=['batch', 'leiden_r05', 'leiden_r10'], add_outline=False, legend_loc='right margin', size=20, color_map='CMRmap')
 
-sc.tl.rank_genes_groups(test3, 'leiden_r05', method='wilcoxon', pts=True)
+sc.tl.rank_genes_groups(test3, 'leiden_r05', method='wilcoxon', pts=True) # key_added=''
 sc.pl.rank_genes_groups(test3, n_genes=5, sharey=False)
 sc.pl.rank_genes_groups_heatmap(test3, n_genes=10, min_logfoldchange=2, cmap='cividis', show_gene_labels=True)
 
@@ -175,7 +174,25 @@ reordered = ('Endothelial cells', 'Smooth muscle cells', 'Fibroblasts', 'B cells
 #reordered = ('Endothelial cells', 'Smooth muscle cells', 'Fibroblasts', 'B cells', 'Macrophages', 'T cells')
 test3.obs['celltype'] = test3.obs['celltype'].cat.reorder_categories(list(reordered), ordered=True)
 
-sc.pl.umap(test3, color=['celltype'], add_outline=False, legend_loc='right margin', size=30, color_map='CMRmap', palette='Paired')
+sc.pl.umap(test3, color=['celltype'], add_outline=False, legend_loc='right margin', size=30, color_map=cmap, palette='Paired')
+
+leiden_to_celltype2_dict = {'0': 'VSMC_1',
+'1': 'VSMC_2',
+'2': 'VSMC_3',
+'3': 'FB_1',
+'4': 'VSMC_4',
+'5': 'EC_1',
+'6': 'FB_2',
+'7': 'EC_2',
+'8': 'VSMC_5',
+'9': 'FB_3',
+'10': 'B-lympho',
+'11': 'M\u03A6',
+'12': 'T-lmpho',
+'13': 'Neuronal'}
+test3.obs['celltype2'] = test3.obs['leiden_r05'].map(lambda x: leiden_to_celltype2_dict[x]).astype('category')
+sc.pl.umap(test3, color=['celltype2'], add_outline=False, legend_loc='on data', size=30, color_map=cmap, palette='tab20')
+
 
 # Imputed expression matrix using MAGIC
 import magic
@@ -195,7 +212,18 @@ dp = sc.pl.dotplot(test3, celltype_marker, layer='magic', groupby='celltype', re
 dp.add_totals(size=1.5, color=['#a6cee3', '#b2df8a', '#fb9a99', '#ff7f00', '#6a3d9a', '#b15928']).legend(colorbar_title='log(SizeFactorNormlized+1)', width=1.5, show_size_legend=False, show_colorbar=False).style(cmap='winter', dot_edge_color='black', dot_edge_lw=1, size_exponent=1.5, grid=True, x_padding=0.4, y_padding=0.6).swap_axes().show()
 sc.pl.StackedViolin(test3, celltype_marker, layer='magic', groupby='celltype')
 #test3.write(filename="/data/Projects/phenomata/01.Projects/11.Vascular_Aging/03.Scanpy/test3.h5ad")
-#test3 = sc.read_h5ad("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/03.Scanpy/test3.h5ad")
+
+sc.pl.matrixplot(test3, celltype_marker, layer='magic', groupby='leiden_r05', dendrogram=True, cmap=cmap, standard_scale='var', colorbar_title='column scaled\nexpression')
+sc.pl.matrixplot(test3, celltype_marker, layer='magic', groupby='celltype', dendrogram=True, cmap=cmap, standard_scale='var', colorbar_title='column scaled\nexpression')
+
+# Using Different celltype markers
+celltype_marker2 = {'EC': ['Pecam1', 'Cdh5', 'Vwf', 'Nos3'],
+'SMC': ['Acta2', 'Tagln', 'Cnn1', 'Cnn2'],
+'FB': ['Dpt', 'Lum', 'Col1a1', 'Pdgfra'],
+'B cells': ['Ighm', 'Cd19'],
+'MΦ':['Cd14', 'Cd68'],
+'T cells':['Cd3d', 'Cd3g']}
+sc.pl.matrixplot(test3, celltype_marker2, layer='magic', groupby='celltype', dendrogram=False, cmap=cmap, standard_scale='var', colorbar_title='Scaled\nexpression', swap_axes=True)
 
 # Only Endothelial cells
 import rpy2.rinterface_lib.callbacks
@@ -295,6 +323,11 @@ sc.tl.rank_genes_groups(test3_endo, 'endo_leiden_r05', method='wilcoxon', pts=Tr
 sc.pl.rank_genes_groups_heatmap(test3_endo, n_genes=10, key='endo_leiden_r05_rank_genes_groups', show_gene_labels=True, min_logfoldchange=1, dendrogram=False, cmap='cividis')
 # 아래와 같이 groups을 지정하면 이 group의 순서대로 column이 배치됨 !!!!!!!!!!!!!!!!!!!!!!!! ==> 아닌 것 같다 다시 한 번 확인.
 sc.pl.rank_genes_groups_heatmap(test3_endo, n_genes=10, key='endo_leiden_r05_rank_genes_groups', groups=['1', '3', '5', '4', '2', '0'], show_gene_labels=True, min_logfoldchange=1, dendrogram=False, cmap='cividis')
+
+#### endo_leiden_r05의 4,5는 아래와 같이 정리 ####
+ec_others = {'Pericyte': ['Rgs5', 'Cspg4', 'Kcnj8', 'Des'], 'Lymphatic EC': ['Reln', 'Flt4'], 'Vasa Vasorum': ['Ackr1', 'Lrg1']}
+sc.pl.matrixplot(test3_endo, ec_others, layer='magic', groupby='endo_leiden_r05', dendrogram=False, cmap=cmap, standard_scale='var', colorbar_title='Scaled\nexpre
+ssion', return_fig=False, var_group_rotation=45)
 
 
 
@@ -657,10 +690,6 @@ test3_endo_MAGIC.X = test3_endo_MAGIC.layers['scran_log1p']
 test3_endo_MAGIC = magic.MAGIC().fit_transform(test3_endo_MAGIC)
 test3_endo.layers['magic'] = test3_endo_MAGIC.X
 del test3_endo_MAGIC
-
-
-test3_endo = sc.read_h5ad("/data/Projects/phenomata/01.Projects/11.Vascular_Aging/03.Scanpy/test3_endo.h5ad")
-test3_endo.layers['magic'] = test3_endo_MAGIC.X
 sc.pl.umap(test3_endo, layer='magic', color=['Pecam1'], add_outline=False, legend_loc='right margin', size=150, color_map='CMRmap')
 
 
@@ -727,7 +756,78 @@ metrics(data = val_results, truth, estimate)
 
 
 
+#### endo_leiden_r05 '4', '5' removal
+test3_endoclean = anndata.AnnData(X=test3_endo[test3_endo.obs['endo_leiden_r05'].isin(['0', '1', '2', '3'])].layers['counts'], obs=test3_endo[test3_endo.obs['endo_leiden_r05'].isin(['0', '1', '2', '3'])].obs, var=test3_endo[test3_endo.obs['endo_leiden_r05'].isin(['0', '1', '2', '3'])].var)
 
+test3_endoclean.layers["counts"] = test3_endoclean.X.copy()
+
+adata_pp = test3_endoclean.copy()
+sc.pp.normalize_per_cell(adata_pp, counts_per_cell_after=1e6)
+sc.pp.log1p(adata_pp)
+sc.tl.pca(adata_pp, n_comps=15) ## 여기서 이 n_component의 숫자를 늘리면 size_factors를 estimation하는 데 도움이 될까?
+sc.pp.neighbors(adata_pp)
+sc.tl.leiden(adata_pp, key_added='groups', resolution=0.5)
+input_groups = adata_pp.obs['groups']
+
+data_mat = test3_endoclean.X.T
+
+%%R -i data_mat -i input_groups -o size_factors
+size_factors = BiocGenerics::sizeFactors(computeSumFactors(SingleCellExperiment::SingleCellExperiment(list(counts=data_mat)), clusters=input_groups, min.mean=0.1))
+
+
+
+del adata_pp
+test3_endoclean.obs['size_factors'] = size_factors
+
+test3_endoclean.X /= test3_endoclean.obs['size_factors'].values[:, None]
+test3_endoclean.X = scipy.sparse.csr_matrix(test3_endoclean.X) #왜 이게 새로 들어가야될까????? # 아니면 ERRROR 남 (highly_variable_genes에서)
+
+test3_endoclean.layers['scran'] = test3_endoclean.X
+
+sc.pp.log1p(test3_endoclean) # works on anndata.X
+#integrated.X = scipy.sparse.csr_matrix(integrated.X)
+test3_endoclean.layers['scran_log1p'] = test3_endoclean.X
+
+test3_endoclean.raw = test3_endoclean ## ==> log transforamtion 된 것이 raw로 들어가게 됨.
+
+sc.pp.highly_variable_genes(test3_endoclean)
+
+test3_endoclean.var['highly_variable'].value_counts() # 2,891 (2021-12-15)
+
+sc.pp.filter_genes(test3_endoclean, min_cells=0) # integrated.var에 n_cells 추가 ==> test3에서 이루어졌던 n_cells UPDATE
+
+sc.pp.scale(test3_endoclean, max_value=10) # ... as `zero_center=True`, sparse input is densified and may lead to large memory consumption
+# adata.raw.X의 mean 과 std를 output함
+sc.tl.pca(test3_endoclean, n_comps=100, use_highly_variable=True, svd_solver='arpack')
+
+#sce.pp.bbknn default ==> n_pcs=50, neighbors_within_batch=3, trim=None, annoy_n_trees=10,
+sce.pp.bbknn(test3_endoclean, batch_key='batch', n_pcs=20, neighbors_within_batch=5, trim=None) #####
+sc.tl.umap(test3_endoclean, min_dist=0.5, spread=1.0, n_components=2, alpha=1.0, gamma=1.0, init_pos='spectral', method='umap')
+#test3_endoclean.uns['batch_colors'] = ['#2a2b2d', '#2da8d8', '#d9514e']
+sc.tl.leiden(test3_endoclean, resolution=0.5, key_added='endoclean_leiden_r05')
+sc.tl.leiden(test3_endoclean, resolution=1.0, key_added='endoclean_leiden_r10')
+
+test3_endoclean.uns['batch_colors'] = ['#689aff', '#fdbf6f', '#b15928']
+
+sc.pl.umap(test3_endoclean, color=['endo_leiden_r05', 'endoclean_leiden_r05', 'leiden_r05'], add_outline=False, legend_loc='right margin', size=150, color_map='CMRmap')
+
+# Imputed expression matrix using MAGIC
+import magic
+MAGIC = test3_endoclean.copy()
+MAGIC.X = test3_endoclean.layers['scran_log1p']
+MAGIC = magic.MAGIC().fit_transform(MAGIC)
+
+test3_endoclean.layers['magic'] = MAGIC.X
+
+
+
+
+
+for gene in test3_endo.var_names:
+    sc.pl.umap(test3_endo, layer='magic', color=gene, color_map=cmap, legend_loc='on data', add_outline=True, outline_width=(0.01, 0.01), show=False, save='_' + gene + '_test3_endo_magic.png')
+
+for gene in test3.var_names:
+    sc.pl.umap(test3, layer='magic', color=gene, color_map=cmap, legend_loc='on data', add_outline=True, outline_width=(0.01, 0.01), show=False, save='_' + gene + '_test3_magic.png')
 
 
 #### Table generation 2021-09-16
@@ -743,5 +843,5 @@ test3_endo.obs['aging_endo_leiden_r05'] = test3_endo.obs['aging_endo_leiden_r05'
 
 tipcell_markers = ['Kdr', 'Flt4', 'Nrp1', 'Nrp2', 'Pdgfb', 'Dll4', 'Angpt2', 'Apln', 'Unc5b', 'Robo4', 'Plxnd1', 'Efnb2', 'Cxcr4']
 sc.tl.score_genes(test3_endo, tipcell_markers, score_name='tipcell_score', use_raw=True)
-sc.pl.umap(test3_endo, layer='magic', color=tipcell_markers, color_map=cmap, ncols=3)
+
 
